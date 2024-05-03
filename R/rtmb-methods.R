67f80cc4b32b06d0abe358cee9eb2353e0285fd2
Quang Huynh <quang@bluematterscience.com>
2024-05-03 13:17:56 -0700

    Add min.advector and max.advector

--- R/rtmb-methods.R
+++ R/rtmb-methods.R
@@ -26,17 +26,58 @@ sapply2 <- function(X, FUN, ..., USE.NAMES = TRUE) {
   sapply(X, FUN, ..., simplify = "array", USE.NAMES = USE.NAMES)
 }
 
-#' @name matmul
-#' @title Matrix multiplication
+#' @name AD
+#' @title Additional methods for AD types
 #'
-#' @description Matrix product function implemented for mixed AD and non-AD objects. See \link[RTMB]{ADmatrix}.
+#' @description Methods for RTMB AD class
+NULL
+
+#' @describeIn AD Matrix product function implemented for mixed AD and non-AD objects with `colSums(x * y)`. See \link[RTMB]{ADmatrix}.
 #'
 #' @param x AD object
-#' @param y Matrix
-#' @aliases %*%,ad,matrix-method
+#' @param y Non-AD matrix
+#' @aliases %*%,ad,matrix-method matmul
 #' @keywords internal
 setMethod("%*%",
           signature("ad", "matrix"),
           function(x, y) {
             colSums(x * y)
           })
+
+#' @describeIn AD Finds the maximum value using [CondExpGt()] over a loop
+#' @aliases max.advector max
+#' @param ... Objects of class advector
+#' @param na.rm Not used
+#' @export
+#' @export max.advector
+max.advector <- function(..., na.rm) {
+  oldval <- TapeConfig()["comparison"]
+  on.exit(TapeConfig(comparison = oldval))
+  TapeConfig(comparison = "allow")
+
+  dots <- list(...)
+  #c <- ADoverload(x = "c")
+  x <- do.call(c, dots)
+
+  xout <- x[1]
+  for (i in 2:length(x)) xout <- CondExpGt(xout, x[i-1], xout, x[i-1])
+  asS4(xout)
+}
+
+#' @describeIn AD Finds the minimum value using [CondExpLt()] over a loop
+#' @aliases min.advector min
+#' @export
+#' @export min.advector
+min.advector <- function(..., na.rm) {
+  oldval <- TapeConfig()["comparison"]
+  on.exit(TapeConfig(comparison = oldval))
+  TapeConfig(comparison = "allow")
+
+  dots <- list(...)
+  #c <- ADoverload(x = "c")
+  x <- do.call(c, dots)
+
+  xout <- x[1]
+  for (i in 2:length(x)) xout <- CondExpLt(xout, x[i-1], xout, x[i-1])
+  asS4(xout)
+}
