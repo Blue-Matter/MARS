b4fe2c85a3b4541e961892f139b4cd400ac46870
Quang Huynh <quang@bluematterscience.com>
2024-05-06 16:58:52 -0700

    Add show method for MARSassess

--- R/rtmb-methods.R
+++ R/rtmb-methods.R
@@ -81,3 +81,50 @@ min.advector <- function(..., na.rm) {
   for (i in 2:length(x)) xout <- CondExpLt(xout, x[i-1], xout, x[i-1])
   asS4(xout)
 }
+
+
+show <- function(object) methods::show(object)
+setMethod("show",
+          signature = "MARSassess",
+          function(object) {
+
+            cat("Number of parameters:", length(object@obj$par))
+            if (length(object@SD) && !is.null(object@SD$gradient.fixed)) {
+              gr <- abs(object@SD$gradient.fixed)
+            } else {
+              gr <- abs(object@obj$gr())
+            }
+            cat("\nMaximum gradient:", round(max(gr), 4))
+
+            if (length(object@SD) && !is.null(object@SD$gradient.fixed)) {
+              gr_large <- gr > 0.1
+
+              if (sum(gr_large)) {
+                gr_report <- gr[gr_large]
+                gr_names <- make_unique_names(object@SD)[gr_large]
+
+                cat("\nParameters with large gradients (> 0.1):\n")
+
+                gr_order <- order(gr_report, decreasing = TRUE)
+
+                x <- data.frame(Gradient = round(gr_report, 4))
+                rownames(x) <- gr_names
+                print(x[gr_order, "Gradient", drop = FALSE])
+              }
+            }
+
+            if (length(object@SD) && !is.null(object@SD$env$hessian)) {
+              h <- object@SD$env$hessian
+              det_h <- det(h)
+              cat(paste("\nDeterminant of Hessian:"), round(det_h, 4))
+
+              zero_rows <- apply(h, 1, function(x) all(x == 0))
+              if (any(zero_rows)) {
+                cat("\nParameters with all zeros in Hessian:\n")
+                par_zero <- names(zero_rows)[zero_rows]
+                for(i in par_zero) cat(i, "\n")
+              }
+            }
+
+            invisible()
+          })
